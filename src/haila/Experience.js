0f2f980bf4039fcbf9f47888d516a14590af4500
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-08-06 23:29:15 +0300

    Adding The Experience page

--- src/haila/Experience.js
+++ src/haila/Experience.js
@@ -1,10 +1,54 @@
-
+import image from '../imgs/king.gif'
 
 
 const Experience=()=>{
     return(
-        <div className="bg-slate-900 text-white flex justify-center items-center">
-            <h1>Welcome to Experience</h1>
+        <div className="bg-slate-900  text-white flex font-serif flex-col justify-center items-center">
+            <h1 className="font-bold text-5xl text-center py-5">My Experiences</h1>
+            <div className='text-lg bg-white shadow-2xl my-4 font-serif text-slate-900 mx-5 shadow-black '>
+                <h1 className="font-bold text-3xl text-center flex items-center justify-center">Internship, in Ethiopian Trade and Market Development Office</h1>
+                <p className='text-center py-1'>Debre Markos, july 2022 - oct 2022</p>
+                <div className='flex justify-between px-10 items-center py-5'>
+                    <img src={image} alt="first images" width={200} height={200}/>
+                    <p className='flex justify-center items-center pl-20'>My internship was a phenomenal experience that enabled me to acquire a diverse skill set 
+                    and gain a comprehensive understanding of web development. I was able to discern the needs 
+                    of users and identify key strategies to enhance website usability.
+                     As I move forward, I remain committed to performing at my absolute best in all future projects.</p>
+                </div>
+            </div>
+            <div className='text-lg bg-white shadow-2xl my-4 font-serif text-slate-900 mx-5 shadow-black '>
+                <h1 className="font-bold text-3xl text-center flex items-center justify-center">Internship, in Ethiopian Trade and Market Development Office</h1>
+                <p className='text-center py-1'>Debre Markos, july 2022 - oct 2022</p>
+                <div className='flex justify-between px-10 items-center py-5'>
+                    <img src={image} alt="first images" width={200} height={200}/>
+                    <p className='flex justify-center items-center pl-20'>My internship was a phenomenal experience that enabled me to acquire a diverse skill set 
+                    and gain a comprehensive understanding of web development. I was able to discern the needs 
+                    of users and identify key strategies to enhance website usability.
+                     As I move forward, I remain committed to performing at my absolute best in all future projects.</p>
+                </div>
+            </div>
+            <div className='text-lg bg-white shadow-2xl my-4 font-serif text-slate-900 mx-5 shadow-black '>
+                <h1 className="font-bold text-3xl text-center flex items-center justify-center">Internship, in Ethiopian Trade and Market Development Office</h1>
+                <p className='text-center py-1'>Debre Markos, july 2022 - oct 2022</p>
+                <div className='flex justify-between px-10 items-center py-5'>
+                    <img src={image} alt="first images" width={200} height={200}/>
+                    <p className='flex justify-center items-center pl-20'>My internship was a phenomenal experience that enabled me to acquire a diverse skill set 
+                    and gain a comprehensive understanding of web development. I was able to discern the needs 
+                    of users and identify key strategies to enhance website usability.
+                     As I move forward, I remain committed to performing at my absolute best in all future projects.</p>
+                </div>
+            </div>
+            <div className='text-lg bg-white shadow-2xl my-4 font-serif text-slate-900 mx-5 shadow-black '>
+                <h1 className="font-bold text-3xl text-center flex items-center justify-center">Internship, in Ethiopian Trade and Market Development Office</h1>
+                <p className='text-center py-1'>Debre Markos, july 2022 - oct 2022</p>
+                <div className='flex justify-between px-10 items-center py-5'>
+                    <img src={image} alt="first images" width={200} height={200}/>
+                    <p className='flex justify-center items-center pl-20'>My internship was a phenomenal experience that enabled me to acquire a diverse skill set 
+                    and gain a comprehensive understanding of web development. I was able to discern the needs 
+                    of users and identify key strategies to enhance website usability.
+                     As I move forward, I remain committed to performing at my absolute best in all future projects.</p>
+                </div>
+            </div>
         </div>
     )
 }
