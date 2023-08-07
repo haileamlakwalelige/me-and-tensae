df75f96322db7084ba7ecd8ca8a065586313e943
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-08-07 11:57:56 +0300

    updating the Navbar

--- src/haila/Navbars.js
+++ src/haila/Navbars.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa';
+import logo from '../imgs/logo.png';
 
 
 const Navbars = () => {
@@ -8,10 +9,10 @@ const Navbars = () => {
     <div className='pt-0 top-0  font-serif shadow-2xl bg-white'>
      <div className='flex justify-between py-4'>
         <div className='pl-10 font-bold'>
-            <h1 className='font-bold text-5xl cursor-pointer hover:text-[#1de3f1] pr-4'>Kingo</h1>
+            <img src={logo} alt="Haileamlak Waleligne Logo" />
         </div>
         
-        <div className="pt-3 md:flex hidden ">
+        <div className="pt-3 lg:flex hidden ">
             <ul className='flex pr-10'>
                 <li className='font-bold text-xl px-3 hover:text-[#cbcb2c] hover:border-b-4 border-green-500 duration-300 ease-linear cursor-pointer'>Home</li>
                 <li className='font-bold text-xl px-3 hover:text-[#cbcb2c] hover:border-b-4 border-green-500 duration-300 ease-linear cursor-pointer'>About</li>
@@ -20,7 +21,7 @@ const Navbars = () => {
                 <li className='font-bold text-xl px-3 hover:text-[#cbcb2c] hover:border-b-4 border-green-500 duration-300 ease-linear cursor-pointer'>Contact</li>
             </ul>
         </div> {show ? (
-            <div className="pt-3 mt-32 -ml-32  md:hidden block">
+            <div className="pt-3 mt-32 -ml-32 h-screen  lg:hidden block">
             <ul className='block pr-10'>
                 <li className='font-bold text-xl py-3 hover:text-[#cbcb2c] hover:border-b-4 border-green-500 duration-300 ease-linear cursor-pointer'>Home</li>
                 <li className='font-bold text-xl py-3 hover:text-[#cbcb2c] hover:border-b-4 border-green-500 duration-300 ease-linear cursor-pointer'>About</li>
@@ -32,12 +33,12 @@ const Navbars = () => {
         </div>):null}
         
         <div className=' mr-10 rounded-lg cursor-pointer flex justify-between'>
-        <div className="md:hidden pr-4 mt-6" onClick={()=>setShow(!show)}>
+        <div className="lg:hidden pr-4 mt-6" onClick={()=>setShow(!show)}>
         {show ? <FaTimes className="text-black font-bold text-2xl " />
         :
         <FaBars className="text-black font-bold text-2xl " />}
         </div>
-            <h1 className='hidden md:block bg-green-500 m-4 w-32 h-10 text-center rounded-lg pt-2 font-bold text-lg hover:bg-[#cbcb2c] text-black hover:text-green-500'>Get Start</h1>
+            <h1 className='hidden lg:block bg-green-500 m-4 w-32 h-10 text-center rounded-lg pt-2 font-bold text-lg hover:bg-[#cbcb2c] text-black hover:text-green-500'>Get Start</h1>
         </div>
         </div>
     </div>
