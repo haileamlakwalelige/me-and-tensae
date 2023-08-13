da051c1c7dd741989c9c59a97fa74815385f7915
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-08-13 15:51:16 +0300

    Updating some features

--- src/haila/Footers.js
+++ src/haila/Footers.js
@@ -1,11 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import {FaFacebook, FaTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa';
+import {FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa';
 import {ImMail} from 'react-icons/im';
 
 const Footers=()=>{
   return(
-    <div className='bg-[#01013ae8] h-[110vh] overflow-y-hidden sm:h-[50vh] shadow-2xl font-serif text-white'>
+    <div className='bg-[#01013ae8] pb-1 md:pb-auto overflow-y-hidden sm:h-[52vh] shadow-2xl font-serif text-white'>
       <div className="bg-[#01013ae8] font-serif shadow-2xl pt-10 justify-center block bottom-0 sticky pb-16">
         <div className="pt-4">
           <h1 className="font-bold text-3xl pb-4 text-center cursor-pointer hover:text-[#04b4fa]"> Haileamlak Waleligne</h1>
@@ -31,24 +31,32 @@ const Footers=()=>{
               </a>
             </ul>
           </div>
-          <div className="flex justify-center">
-            <a href="https://www.haileamlakwaleligne3910@gmail.com">
+          <div className="flex justify-center flex-col mb-1 sm:flex-row">
+            <div className="flex justify-evenly">
+               <a  href="https://www.haileamlakwaleligne3910@gmail.com" className="my-3 cursor-pointer">
               <ImMail size={60} className="font-bold mx-4 p-2 text-black hover:text-[#04b4fa] bg-white rounded-full h-16 w-16"/>
             </a>
-            <a href="https://www.facebook.com/profile.php?id=100026113192333" className="cursor-pointer">
+            <a  href="https://github.com/haileamlakwalelige" className="my-3 cursor-pointer">
+              <FaGithub size={60} className="font-bold mx-4 p-2 text-black hover:text-[#04b4fa] bg-white rounded-full h-16 w-16"/>
+            </a>
+            <a  href="https://www.facebook.com/profile.php?id=100026113192333" className="my-3 cursor-pointer">
             <FaFacebook size={60} className="font-bold mx-4 p-2 text-black hover:text-[#04b4fa] bg-white rounded-full h-16 w-16"/>
             </a>
-            <a href="https://twitter.com/Haileopia" className="cursor-pointer">
+            </div>
+            <div className="flex justify-evenly">
+              <a  href="https://twitter.com/Haileopia" className="my-3 cursor-pointer">
             <FaTwitter size={60} className="font-bold mx-4 text-black hover:text-[#04b4fa] bg-white rounded-full p-2 h-16 w-16"/>
             </a>
-            <a href="https://www.instagram.com/hailaopia/" className="cursor-pointer">
+            <a  href="https://www.instagram.com/hailaopia/" className="my-3 cursor-pointer">
             <FaInstagram size={60} className="font-bold mx-4 text-black hover:text-[#04b4fa] bg-white rounded-full p-2 h-16 w-16"/>
             </a>
-            <a href="https://www.linkedin.com/in/haileamlak-waleligne/" className="cursor-pointer">
+            <a  href="https://www.linkedin.com/in/haileamlak-waleligne/" className="my-3 cursor-pointer">
               <FaLinkedin size={60} className="font-bold mx-4 text-black hover:text-[#04b4fa] bg-white rounded-full p-2 h-16 w-16"/>
             </a>
+            </div>
+            
           </div>
-          <p className="text-center pt-6 font-mono text-lg mb-10">Copyright ©2023 All rights reserved | This Website is made by Haileamalak Waleligne</p>
+          <p className="text-center pt-6 font-mono text-lg">Copyright ©2023 All rights reserved | This Website is made by Haileamalak Waleligne</p>
         </div>
       </div>
     </div>
